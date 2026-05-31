c2fe3ff935e5d764a57779102c9587a6128f2147
PRINCEANNANG <annangokoprince@gmail.com>
2026-05-31 23:01:08 +0000

    update

--- initials.py
+++ initials.py
@@ -1,7 +1,24 @@
 
 def main():
   pass #code goes here
+ # Print the initials P, O, A in large 12-wide x 10-high block letters composed
+    # of their respective capital letters and periods. Each letter is followed
+    # by a 4-asterisk period block. Three period columns separate each section.
 
+    print()
+    print("...POA")
+    print()
+    print("...PPPPPPPPPP...........OOOOOOOO.............AAAA.......")
+    print("...PP......PPP.........OO......OO...........AAAAAA......")
+    print("...PP......PPP........OO........OO.........AA....AA.....")
+    print("...PP......PPP........OO........OO........AA......AA....")
+    print("...PPPPPPPPPP.........OO........OO........AA......AA....")
+    print("...PPPPPPPPPP.........OO........OO.......AAAAAAAAAAAA...")
+    print("...PP.................OO........OO.......AA........AA...")
+    print("...PP.................OO........OO.......AA........AA...")
+    print("...PP............**...OO......OO....**...AA........AA..**")
+    print("...PP............**.....OOOOOOOO....**...AA........AA..**")
+    print()
 
 
 main()
